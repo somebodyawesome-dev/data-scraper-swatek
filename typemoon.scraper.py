e2c621c64d3787a324d1ef85c0e68b5e0d232d26
somebodyawesome-dev <59267562+somebodyawesome-dev@users.noreply.github.com>
2021-07-18 15:55:56 +0100

    added typemoon scraper

---
+++ typemoon.scraper.py
@@ -0,0 +1,57 @@
+
+from bs4 import BeautifulSoup
+import requests
+import json
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import csv
+import time
+
+
+def fetchCharacterData(url):
+    pageSource = requests.get(url).text
+    soup = BeautifulSoup(pageSource, "lxml")
+    mainContainer = soup.find("div", {"id": "content"}).find(
+        "aside", {"role": "region"})
+    characterName = mainContainer.find(
+        "h2", {"data-source": "name"}).get_text()
+    species = [spec["title"] for spec in mainContainer.find(
+        "div", {"data-source": "type"}).find_all("a")] if mainContainer.find(
+        "div", {"data-source": "type"}) else []
+    gender = mainContainer.find("div", {"data-source": "gender"}).find("div").get_text(
+    ) if mainContainer.find("div", {"data-source": "gender"}) else "UKNOWN"
+    height = "".join(mainContainer.find(
+        "div", {"data-source": "height"}).find("div").find_all(text=True, recursive=False)).strip() if mainContainer.find("div", {"data-source": "height"}) else "UKNOWN"
+    weight = "".join(mainContainer.find(
+        "div", {"data-source": "weight"}).find("div").find_all(text=True, recursive=False)).strip() if mainContainer.find("div", {"data-source": "weight"}) else "UKNOWN"
+    origin = "".join(mainContainer.find(
+        "div", {"data-source": "birthp"}).find("div").find_all(text=True, recursive=False)).strip() if mainContainer.find("div", {"data-source": "birthp"}) else "UKNOWN"
+    likes = "".join(mainContainer.find(
+        "div", {"data-source": "likes"}).find("div").find_all(text=True, recursive=False)).strip() if mainContainer.find("div", {"data-source": "likes"}) else "UKNOWN"
+    dislikes = "".join(mainContainer.find(
+        "div", {"data-source": "dislikes"}).find("div").find_all(text=True, recursive=False)).strip() if mainContainer.find("div", {"data-source": "dislikes"}) else "UKNOWN"
+    age = "".join(mainContainer.find(
+        "div", {"data-source": "age"}).find("div").find_all(text=True, recursive=False)).strip() if mainContainer.find("div", {"data-source": "age"}) else "UKNOWN"
+
+    return {"name": characterName,
+            "description": "",
+            "imageURL": "",
+            "age": age,
+            "species": species,
+            "gender": gender,
+            "height": height,
+            "weight": weight,
+            "origin": origin,
+            "likes": likes,
+            "dislikes": dislikes}
+
+
+if __name__ == "__main__":
+    with open("data.txt", mode="a", encoding='utf-8') as file:
+        json.dump(fetchCharacterData(
+            input("url:")), file)
+        file.write("\n")
+
+    pass
